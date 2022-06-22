ff29822b8c00169ad5dfea5fc34a0ee67c840586
mynulislam75 <mynulislam181120@gmail.com>
2022-06-23 04:52:04 +0600

    added commit

---
+++ simple-interest.js
@@ -0,0 +1,6 @@
+function simpleInterest(p,t,r){
+    var simpleInyerestCalculation=(p*t*r)/100;
+    return simpleInyerestCalculation;
+}
+var finalResult=simpleInterest(4500,9.5,6)
+console.log(finalResult)
