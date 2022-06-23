66a8d8f9e17a00a28b770bc4cb8b0084e0c97b94
mynulislam75 <mynulislam181120@gmail.com>
2022-06-23 18:08:05 +0600

    added commit

--- vowel-in-array.js
+++ vowel-in-array.js
@@ -1,17 +1,26 @@
-
+// DECLARING A FUNCTION
 function vowelIs(givenArray) {
     let vowels = ["a", "e", "i", "o", "u"];
+    let newVowelArr = [""]
+    let newConstantArr = [""]
     for (let i = 0; i < givenArray.length; i++) {
         if (givenArray[i] == vowels[0] || givenArray[i] == vowels[1] || givenArray[i] == vowels[2] ||
             givenArray[i] == vowels[3] || givenArray[i] == vowels[4]) {
             console.log("It's a vowel", givenArray[i])
+            newVowelArr[i] = givenArray[i]
         }
         else {
-           console.log("Not a vowel", givenArray[i])
+            console.log("Not a vowel", givenArray[i])
+            newConstantArr[i] = givenArray[i]
         }
+    }
+    return {
+        vowelArr: newVowelArr,
+        constArr: newConstantArr
     }
 }
 
-let myArray = ["a", "j", "i", "p", "w"]
+let myArray = ["a", "j", "i", "p", "w", "i", "y"]
 let finalResult = vowelIs(myArray)
-// console.log(finalResult)
+console.log(finalResult)
+
